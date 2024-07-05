712e5ee50a7726396a542aacced6f7e8bed31f42
Sparsh Mishra <iamsparshmishra@gmail.com>
2024-07-05 09:29:12 +0530

    jscommits

--- JavaScript/Logicals.js
+++ JavaScript/Logicals.js
@@ -17,12 +17,12 @@ if (password.length >= 6 && password.indexOf(' ') === -1) {
 // 10-65 $20
 // 65+ free
 
-const age = 100;
-if ((age >= 0 && age < 5) || age >= 65) {
+const agee = 100;
+if ((agee >= 0 && agee < 5) || agee >= 65) {
     console.log("FREE");
-} else if (age >= 5 && age < 10) {
+} else if (agee >= 5 && agee < 10) {
     console.log("$10")
-} else if (age >= 10 && age < 65) {
+} else if (agee >= 10 && agee < 65) {
     console.log("$20")
 } else {
     console.log("INVALID AGE!")
