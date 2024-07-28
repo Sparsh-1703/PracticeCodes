4526592e8b8129509cbd120423b3e9cec6a2796f
Sparsh Mishra <iamsparshmishra@gmail.com>
2024-07-28 23:16:51 +0530

    C

---
+++ C Files/Operators.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main (){
+    //ARITHEMATIC OPERATORS
+
+    // +  (addition)
+    // -  (substraction)
+    // *  (multiplication)
+    // /  (division)
+    // %  (modulus)
+    // ++ (increment)
+    // -- (decrement)
+
+    int x = 5;
+    int y = 2;
+
+    // int z = x + y;
+    // int z = x - y;
+    // int z = x * y;
+    // float z = x / (float) y;
+    // int z = x % y;
+
+    x++;
+    y--;
+
+    printf("%d", y);
+    return 0;    
+}
