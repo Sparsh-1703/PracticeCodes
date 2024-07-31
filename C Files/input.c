63c0b985a14b7d4fea0e508c6842694582540cfe
Sparsh Mishra <iamsparshmishra@gmail.com>
2024-07-31 06:02:12 +0530

    C

--- C Files/input.c
+++ C Files/input.c
@@ -3,7 +3,9 @@ int main(){
     char name[25]; //bytes
     int age;
     printf("What is your name? ");
-    scanf("%s", &name);
+    // scanf("%s", &name);
+    fgets(name, 25, stdin);
+    name[strlen(name)-1] = '\0';
     printf("Hello %s, how are you? ", name);
 
     printf("\nHow old are you? ");
