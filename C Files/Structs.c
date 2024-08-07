0cdeea2e8d52e7f037bc552a22c1b1b20cb7a6f9
Sparsh Mishra <iamsparshmishra@gmail.com>
2024-08-07 21:53:40 +0530

    C

---
+++ C Files/Structs.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+struct Player{                        //CAN BE OF DIFFERENT DATA TYPES UNLIKE ARRAYS!!
+    char name[12];
+    int score;
+};
+int main(){
+    // struct = collection of relted members ("variables")
+    //          they can be of different data types
+    //          listed under one name in a block of memory
+    //          VERY SIMILAR to classes in other languages (but no methods)
+
+    struct Player player1;
+    struct Player player2;  
+
+    strcpy(player1.name, "Sigmeshhh");
+    player1.score = 4;
+
+    strcpy(player2.name, "Maxxyyy");
+    player2.score = 5;
+
+    printf("%s\n", player1.name);
+    printf("%d\n", player1.score);
+
+    printf("%s\n", player2.name);
+    printf("%d\n", player2.score);
+
+    return 0;
+}
---
+++ C Files/Typedef.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    //typedef = reserved keyword that gives an existing database a "nickname" .
+    
+    return 0;
+}
