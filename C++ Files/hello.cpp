0d5909ab614edbc9cd9c543f357515a96071d5fe
Sparsh Mishra <iamsparshmishra@gmail.com>
2024-08-09 00:05:30 +0530

    Cpp

---
+++ C++ Files/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+    std::cout << "I like Pizza!" <<'\n';
+    std::cout << "It's really good." <<std::endl;
+    std::cout << "It's reallyyyyy good." <<std::endl;
+    return 0;
+}
