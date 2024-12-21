c0b6f68305f49506b7a8caf2c467a8411733fb2d
Sparsh Mishra <iamsparshmishra@gmail.com>
2024-12-21 22:39:13 +0530

    py

--- Python/loops.py
+++ Python/loops.py
@@ -11,3 +11,12 @@ total = 0
 for i in range(1,101):
     total += i
 print(total)
+for i in range(1,101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("Fizzbuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
--- Python/passwordGen.py
+++ Python/passwordGen.py
@@ -4,29 +4,26 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?: ")) 
-nr_symbols = int(input(f"How many symbols would you like?: "))
-nr_numbers = int(input(f"How many numbers would you like?: "))
 
-#Hard Level:
+try:
+    nr_letters = int(input("How many letters would you like in your password?: ")) 
+    nr_symbols = int(input("How many symbols would you like?: "))
+    nr_numbers = int(input("How many numbers would you like?: "))
+except ValueError:
+    print("Please enter valid numbers.")
+    exit()
 password_list = []
 
-for char in range(1, nr_letters + 1):
-  password_list.append(random.choice(letters))
+for _ in range(nr_letters):
+    password_list.append(random.choice(letters))
 
-for char in range(1, nr_symbols + 1):
-  password_list += random.choice(symbols)
+for _ in range(nr_symbols):
+    password_list.append(random.choice(symbols))
 
-for char in range(1, nr_numbers + 1):
-  password_list += random.choice(numbers)
-
-print(password_list)
+for _ in range(nr_numbers):
+    password_list.append(random.choice(numbers))
 random.shuffle(password_list)
-print(password_list)
-
-password = ""
-for char in password_list:
-  password += char
+password = ''.join(password_list)
 
 print(f"Your password is: {password}")
 
